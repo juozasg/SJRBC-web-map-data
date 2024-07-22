cab6763868bc5973033c7c741e80015f69251d36
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-07-22 20:29:58 +0300

    index usgs sites

--- build-indexes.rb
+++ build-indexes.rb
@@ -7,6 +7,8 @@ puts "Yay" if RGeo::Geos.capi_supported?
 file = 'geojson/huc10.geojson'
 features = RGeo::GeoJSON.decode(File.read(file))
 rows = CSV.read('sites/sites.csv', headers: true)
+rows2 = CSV.read('sites/usgs.csv', headers: true)
+
 
 indexRows = ['siteId,huc10']
 
@@ -19,7 +21,14 @@ rows.each do |row|
 	# puts index
 	indexRows << index
 end
+# TODO: nope
+rows2.each do |row|
+	point = factory.point(row['lon'].to_f, row['lat'].to_f)
+	matching_feature = features.find { |feature| feature.geometry.contains?(point) }
+	huc10 = matching_feature ? matching_feature['huc10'] : ''
+	index = row['siteId'] + ',' + huc10
+	# puts index
+	indexRows << index
+end
 
 File.write('indexes/sites.csv', indexRows.join("\n"))
-
-
