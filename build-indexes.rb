b94ef35be134e83b4ff9e202822175cef4a88d4c
Juozas Gaigalas <juozasgaigalas@gmail.com>
2024-07-23 13:18:29 +0300

    index all geometries

--- build-indexes.rb
+++ build-indexes.rb
@@ -4,31 +4,45 @@ require 'rgeo/geo_json'
 
 puts "Yay" if RGeo::Geos.capi_supported?
 
-file = 'geojson/huc10.geojson'
-features = RGeo::GeoJSON.decode(File.read(file))
-rows = CSV.read('sites/sites.csv', headers: true)
-rows2 = CSV.read('sites/usgs.csv', headers: true)
+huc8File = 'geojson/huc8.geojson'
+huc10File = 'geojson/huc10.geojson'
+huc12File = 'geojson/huc12.geojson'
+statesFile = 'geojson/basin-states.geojson'
+countiesFile = 'geojson/counties.geojson'
+
+indexes = %w(huc8 huc10 huc12 state county)
+idColumns = %w(huc8 huc10 huc12 statefp countyfp)
 
 
-indexRows = ['siteId,huc10']
+siteFiles = Dir.glob('sites/**/*.csv')
+features = {}
 
-factory = RGeo::Geographic.spherical_factory
-rows.each do |row|
-	point = factory.point(row['lon'].to_f, row['lat'].to_f)
-	matching_feature = features.find { |feature| feature.geometry.contains?(point) }
-	huc10 = matching_feature ? matching_feature['huc10'] : ''
-	index = row['siteId'] + ',' + huc10
-	# puts index
-	indexRows << index
+indexes.each do |index|
+	file = 'geojson/' + index + '.geojson'
+	features[index] = RGeo::GeoJSON.decode(File.read(file))
 end
-# TODO: nope
-rows2.each do |row|
-	point = factory.point(row['lon'].to_f, row['lat'].to_f)
-	matching_feature = features.find { |feature| feature.geometry.contains?(point) }
-	huc10 = matching_feature ? matching_feature['huc10'] : ''
-	index = row['siteId'] + ',' + huc10
-	# puts index
-	indexRows << index
+
+
+def rowToPoint(row)
+	RGeo::Geographic.spherical_factory.point(row['lon'].to_f, row['lat'].to_f)
+end
+
+indexRows = ['siteId,' + indexes.join(',')]
+siteFiles.each do |file|
+	rows = CSV.read(file, headers: true)
+	rows.each do |row|
+		rowColumns = [row['siteId']]
+
+		point = rowToPoint(row)
+		indexes.each_with_index do |indexName, i|
+			matching_feature = features[indexName].find { |feature| feature.geometry.contains?(point) }
+			idColumn = idColumns[i]
+			id = matching_feature ? matching_feature[idColumn] : ''
+			rowColumns << id
+		end
+
+		indexRows << rowColumns.join(',')
+	end
 end
 
 File.write('indexes/sites.csv', indexRows.join("\n"))
