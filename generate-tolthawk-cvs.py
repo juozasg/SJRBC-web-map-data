46841b333557e14b669458e7eacb610230d9c2a2
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-06-20 20:19:10 +0300

    tt csv wip

--- generate-tolthawk-cvs.py
+++ generate-tolthawk-cvs.py
@@ -1,20 +1,27 @@
 import requests
+from datetime import datetime
 
 # Region ID for sensors
 regionId = 49
+startDate = '20150101'
+startDate = '20250620'
+endDate = datetime.now().strftime('%Y%m%d')
+dateRange = f"{startDate}0000-{endDate}2359"
 
 def read_token():
     with open('tolthawk-token', 'r') as f:
         token = f.read().strip()
         return token
 
-def get_sensor_status(token, region_id):
-    url = f"https://sensors.tolthawk.com/api/mobile/LocationsStatus/{region_id}"
+headers = {
+    "Content-Type": "application/json",
+    "Token": read_token()
+}
 
-    headers = {
-        "Content-Type": "application/json",
-        "Token": token
-    }
+
+
+def get_sensor_status(region_id):
+    url = f"https://sensors.tolthawk.com/api/mobile/LocationsStatus/{region_id}"
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
@@ -24,13 +31,34 @@ def get_sensor_status(token, region_id):
     print(f"Successfully fetched data for {len(data)} sensors")
     return data
 
+def get_water_levels(sensor_id):
+    url = f"https://sensors.tolthawk.com/api/mobile/WaterLevels/{sensor_id}/{dateRange}"
+    print(f"Fetching tolthawk-{sensor_id} from {url}")
 
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    # Parse response JSON
+    data = response.json()
+    print(f"Successfully fetched readings for sensor {sensor_id}")
+    return data
 
 # Main execution
-if __name__ == "__main__":
-    api_token = read_token()
+# TODO: collect all readings for each day and write mean water level to tolthawk.csv
 
-    sensor_status = get_sensor_status(api_token, regionId)
+sensor_status = get_sensor_status(regionId)
+for sensor in sensor_status:
+    sensor_id = sensor['Lid']
+    # print(f"Sensor ID: {sensor_id}")
+    readings = get_water_levels(sensor_id)
+    print(f"Water levels for {sensor_id}:")
+    for reading in readings:
+        datetime = reading['DT']
+        waterlevel = reading['WLV']
+        # print(reading)
+    exit(0)
 
-    print(sensor_status)
+
+    # print(sensor_status)
+    # sensor_id = 1001  # Example sensor ID, replace with actual ID
 
