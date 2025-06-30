e3e830d01d938db8eea3160bc21daddf4e7c4efb
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-06-30 19:21:32 +0300

    15 min usgs

--- generate-usgs-15-min.py
+++ generate-usgs-15-min.py
@@ -1,6 +1,10 @@
 # cache USGS NWIS daily data in CSV format under datasets
 
 from datetime import datetime
+
+import pytz
+
+import dateutil
 import requests
 import json
 import csv
@@ -10,15 +14,15 @@ from collections import defaultdict
 import dates_with_records as dwr
 
 
-def varname(variable_name):
+def varname(name: str):
     # find substring in variable_name and return a more readable name
-    if 'Temperature' in variable_name:
+    if 'Temperature' in name:
         return 'temp'
-    elif 'Streamflow' in variable_name:
+    elif 'Streamflow' in name:
         return 'flow'
-    elif 'Specific conductance' in variable_name:
+    elif 'Specific conductance' in name:
         return 'spc'
-    elif 'Dissolved oxygen' in variable_name:
+    elif 'Dissolved oxygen' in name:
         return 'do'
     else:
         return None
@@ -26,13 +30,15 @@ def varname(variable_name):
 
 startDate = '2023-03-01'
 endDate = datetime.now().strftime('%Y-%m-%d')
+# endDate = '2023-03-02'
+
 # endDate = '2011-05-01'  # For testing, use a fixed date
 
 ids = ['04096405','04096515','04097500','040975299','04097540','04099000','04100500','04101000','04101500','04101800','04102500','04099750']
 ids = ','.join(ids)
 # ids = '04101500'  # For testing with a single site
 
-url = f'https://waterservices.usgs.gov/nwis/iv/?format=json&sites={ids}&statCd=00003&siteStatus=all&startDT={startDate}&endDT={endDate}'
+url = f'https://waterservices.usgs.gov/nwis/iv/?format=json&sites={ids}&siteStatus=all&startDT={startDate}&endDT={endDate}'
 print(f"URL: {url}")
 
 # Create datasets directory if it doesn't exist
@@ -46,12 +52,15 @@ if response.status_code == 200:
 
     # Create a nested dictionary to store data by site_id and date
     # Structure: site_data[site_id][date] = {'flow': value, 'temp': value, etc.}
-    site_data = defaultdict(lambda: defaultdict(dict))
+    site_data: dict[str, dict[datetime, dict[str, float]]] = dict()
 
     # Extract timeSeries data
     for series in data['value']['timeSeries']:
         site_id = series['sourceInfo']['siteCode'][0]['value']
-        site_id = 'usgs-' + site_id  # Prefix site ID with 'usgs'
+        site_id: str = 'usgs-' + site_id  # Prefix site ID with 'usgs'
+
+        if not (site_id in site_data):
+            site_data[site_id] = dict()
 
         variable_name = series['variable']['variableName']
         variable_type = varname(variable_name)
@@ -61,10 +70,13 @@ if response.status_code == 200:
 
         # Extract values
         for value_entry in series['values'][0]['value']:
-            date_time = value_entry['dateTime']
-            value = value_entry['value']
+            date_time = dateutil.parser.isoparse(value_entry['dateTime']).astimezone(pytz.utc)
+            value = float(value_entry['value'])
 
             # Store the value in our nested dictionary
+            if not (date_time in site_data[site_id]):
+                site_data[site_id][date_time] = dict()
+
             site_data[site_id][date_time][variable_type] = value
             # print(f"Collected: {site_id}: {variable_type} {date_time} {value}")
 
@@ -73,12 +85,7 @@ if response.status_code == 200:
     for site_id in site_data:
         site_data[site_id] = dict(sorted(site_data[site_id].items()))
 
-    unique_dates = dwr.get_unique_dates()
-    # delete dates that do not have records
-    for site_id in list(site_data.keys()):
-        for date in list(site_data[site_id].keys()):
-            if date not in unique_dates:
-                del site_data[site_id][date]
+
     # Open CSV file for writing
     with open('15min/usgs.csv', 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
@@ -91,7 +98,7 @@ if response.status_code == 200:
             for date, variables in dates.items():
                 row = [
                     site_id,
-                    date,
+                    date.strftime("%Y-%m-%dT%H:%M:%SZ"),
                     variables.get('flow', ''),  # Use empty string if variable doesn't exist
                 ]
                 csv_writer.writerow(row)
