a1e59d0cf95e5dd22c5cda2f0b25b9c6ca085e7e
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-07-05 20:09:05 +0300

    init python paths

--- scripts/init-python.sh
+++ scripts/init-python.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+SOURCE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SOURCE_DIR"
+
 python3 -m venv .venv
 . .venv/bin/activate
 python3 -m pip install -r requirements.txt
