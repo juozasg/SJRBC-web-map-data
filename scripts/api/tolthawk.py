f2e6a800fe185de432339a778e95df98bad973be
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-07-07 21:42:47 +0300

    waterlevel flow again

--- scripts/api/tolthawk.py
+++ scripts/api/tolthawk.py
@@ -1,9 +1,8 @@
 import os
 from datetime import datetime
-
 import requests
 
-from .common import Timeseries, TimeseriesRecord
+from api.common import Timeseries, TimeseriesRecord
 
 tolthawk_valid_sensors = [393, 394, 395, 396, 397, 398, 399]
 
@@ -16,7 +15,6 @@ curves: dict[int: [float, float, float]] = {
     397: [ 40.5930, -76_601.0360,  36_137_494.6487],
     398: [-14.8347,  28_530.5057, -13_717_545.2763],
     399: [ 38.4723, -58_123.5123,  21_953_055.762],
-
 }
 
 
@@ -34,25 +32,46 @@ def read_token():
         return token
 
 
-def get_sensor_status():
+def get_region_status():
     region_id = 49
-    url = f"https://sensors.tolthawk.com/api/mobile/LocationsStatus/{region_id}/393"
+    url = f"https://sensors.tolthawk.com/api/mobile/LocationsStatus/{region_id}"
 
     headers = {
         "Content-Type": "application/json",
         "Token": read_token()
     }
+
+    print(f"Fetching from {url}")
     response = requests.get(url, headers=headers)
     response.raise_for_status()
 
     # Parse response JSON
     data = response.json()
-    print(f"Successfully fetched data for {len(data)} sensors")
-    print(data)
+    # print(f"Successfully fetched data for {len(data)} sensors")
+    # print(data)
+    return data
+
+def get_sensor_status(sensor_id: int):
+    region_id = 49
+    url = f"https://sensors.tolthawk.com/api/mobile/LocationsStatus/{region_id}/{sensor_id}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Token": read_token()
+    }
+
+    print(f"Fetching tolthawk-{sensor_id} from {url}")
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    # Parse response JSON
+    data = response.json()
+    # print(f"Successfully fetched data for {len(data)} sensors")
+    # print(data)
     return data
 
 
-def fetch_tolthawk_iv(sensor_id: int, from_dt: datetime, to_dt: datetime) -> Timeseries:
+def fetch_tolthawk_iv(sensor_id: int, from_dt: datetime, to_dt: datetime, debug = False) -> Timeseries:
 
     start = from_dt.strftime('%Y%m%d%H%M')
     end = to_dt.strftime('%Y%m%d%H%M')
@@ -70,6 +89,8 @@ def fetch_tolthawk_iv(sensor_id: int, from_dt: datetime, to_dt: datetime) -> Tim
     response = requests.get(url, headers=headers)
     response.raise_for_status()
 
+    if debug:
+        print(response.json())
     # Parse response JSON
     return api_readings_to_timeseries(response.json())
 
@@ -81,11 +102,13 @@ def api_readings_to_timeseries(readings: list[dict]) -> Timeseries:
         waterlevel = reading['WLV']
         groundheight = reading['GH']
         sensor_id = reading['Lid']
-        flow = height_to_streamflow(sensor_id, groundheight)
+        flow = height_to_streamflow(sensor_id, waterlevel)
         tseries.append(TimeseriesRecord(timestamp=timestamp, flow=flow, height=float(waterlevel)))
     return tseries
 
 if __name__ == "__main__":
-    print(fetch_tolthawk_iv(392, datetime(2024, 1, 1), datetime(2024, 1, 3)))
+    # print(get_region_status())
+    # print(get_sensor_status(393))
+    fetch_tolthawk_iv(395, datetime(2025, 7, 7), datetime(2025, 7, 8), True)
 
 
